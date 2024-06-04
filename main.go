d1ca42746d622c04b7aa2937071388f1cd0e11a4
BPplays <andymalbp@gmail.com>
2024-06-04 15:43:44 -0700

    fucked around with vlans

--- main.go
+++ main.go
@@ -348,7 +348,7 @@ func get_prefix2(ipnet *net.IPNet, vlan int16) string {
 
 	i := 0
     for index := 0; i <= requiredLength; {
-		if index < len(ipv6Prefixrn)-1  {
+		if index >= 0 && index < len(ipv6Prefixrn) {
 			if ipv6Prefixrn[index] != ':' {
 				i +=1
 			}
