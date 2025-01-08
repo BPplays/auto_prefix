c91c30cd5f36a31a718c69db61ef66b78c1e5016
BPplays <andymalbp@gmail.com>
2025-01-08 01:20:21 -0800

    debug

--- main.go
+++ main.go
@@ -178,7 +178,7 @@ func set_ipaddr_bits(addr net.IP, subnet_uint64 uint64, start int, end int) net.
 
 	var addr_bytes []byte
 	addr_bytes = addr.To16()
-	fmt.Printf("addr: %v,addr_bytes: %v\n", addr.String(), sprintBytesAsBinary(addr_bytes))
+	fmt.Printf("addr: %v,\naddr subnet uint64: %v,\naddr_bytes: %v\n\n", addr.String(), subnet_uint64, sprintBytesAsBinary(addr_bytes))
 
 	fmt.Printf("set bits: start: %v, end: %v\n", start, end)
 	for i := end; i >= start; i-- {
